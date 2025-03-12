d653688a1357455f019dce1c5b12453bf7c1077a
Luis Mario <43504168+LuisM711@users.noreply.github.com>
2025-03-12 15:20:33 -0700

    slider

--- frontend/src/app/components/home/home.component.ts
+++ frontend/src/app/components/home/home.component.ts
@@ -1,12 +1,48 @@
 import { Component } from '@angular/core';
+import { MatIcon, MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [MatIconModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
+  images = [
+    'http://localhost:4200/slider/1.jpg',
+    'http://localhost:4200/slider/2.png',
+    'http://localhost:4200/slider/3.jpg',
+  ];
+  currentIndex = 0;
+  autoSlide = true;
+  intervalId: any;
 
+  constructor() {
+    this.startAutoSlide();
+  }
+
+  startAutoSlide() {
+    this.intervalId = setInterval(() => {
+      if (this.autoSlide) {
+        this.nextImage();
+      }
+    }, 3000);
+  }
+
+  stopAutoSlide() {
+    this.autoSlide = false;
+  }
+
+  resumeAutoSlide() {
+    this.autoSlide = true;
+  }
+
+  nextImage() {
+    this.currentIndex = (this.currentIndex + 1) % this.images.length;
+  }
+
+  prevImage() {
+    this.currentIndex = (this.currentIndex - 1 + this.images.length) % this.images.length;
+  }
 }
